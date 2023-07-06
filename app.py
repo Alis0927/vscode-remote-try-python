f2ccedf3cd648b3815445e7ed76d256d4656686a
Alis0927 <132944029+Alis0927@users.noreply.github.com>
2023-07-06 16:46:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
-
+python -m flask run --port 9000 --no-debugger --no-reload
 from flask import Flask
 app = Flask(__name__)
 
